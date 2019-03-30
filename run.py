aea3c69dae2686a783f7e19bbafdd380018bbd5c
rostegg <rostik.nikolaenko@gmail.com>
2019-03-30 22:36:50 +0200

    Prod server support

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from app import create_app
 from app.api import api_v1
+from waitress import serve
 
 app = create_app(os.getenv('FLASK_CONFIG_TYPE') or 'dev')
 app.register_blueprint(api_v1)
@@ -9,4 +10,4 @@ if __name__ == '__main__':
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get("PORT", 5000))
 
-    app.run(host=host, port=port)
+    serve(app, host=host, port=port) if (os.getenv('FLASK_CONFIG_TYPE') == 'prod') else app.run(host=host, port=port)
