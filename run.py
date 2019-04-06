6895572cae6ef2a617f80b646acc5cb83f97ac79
rostegg <rostik.nikolaenko@gmail.com>
2019-04-06 09:17:21 +0300

    Redirect  root url

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
+from flask import redirect
 from app import create_app
 from app.api import api_v1
 from waitress import serve
 
 app = create_app(os.getenv('FLASK_CONFIG_TYPE') or 'dev')
 app.register_blueprint(api_v1)
+
+@app.route('/')
+def root():
+    return redirect('/v1/test')
 
 if __name__ == '__main__':
     host = os.environ.get('IP', '0.0.0.0')
