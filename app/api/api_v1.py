dc78cea1ac08f64d7c5ba859ad5ac5f7b33ff5a8
rostegg <rostik.nikolaenko@gmail.com>
2019-04-06 09:15:04 +0300

    Changed resource path

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -11,7 +11,7 @@ test_response = [{
     'message': 'Hello'
 }]
 
-api_v1 = Blueprint(name='api_v1', import_name=__name__, url_prefix="/api/v1.0")
+api_v1 = Blueprint(name='api_v1', import_name=__name__, url_prefix="/v1")
 
 @api_v1.route('/test',methods=['GET'])
 @cache.cached(timeout=60)
